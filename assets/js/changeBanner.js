e8b9d0d8ebb7e988035e73047ecfff08cac85220
Hector Leubeck <hector.leubeck.lux.renatus@gmail.com>
2025-08-18 20:13:01 -0300

    perf: add image validation to changeBanner function

--- assets/js/changeBanner.js
+++ assets/js/changeBanner.js
@@ -1,7 +1,16 @@
 const banner = document.querySelector(".app__image");
 
-const changeBanner = (src) => {
-  banner.setAttribute("src", `../img/${src}.png`);
+const changeBanner = (banner) => {
+  const images = {
+    focus: "focus",
+    "short-pause": "short-pause",
+    "long-pause": "long-pause",
+  };
+
+  const bannerName = images[banner];
+  if (!bannerName) {
+    return banner.setAttribute("src", `../img/${bannerName}.png`);
+  }
 };
 
 export default changeBanner;

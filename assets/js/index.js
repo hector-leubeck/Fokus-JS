44f6d4192ee47bb865a9a21f75a411ce6da0f528
Hector Leubeck <hector.leubeck.lux.renatus@gmail.com>
2025-08-18 17:22:10 -0300

    perf: Optimize Project structure

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const html = document.querySelector("html");
+const focusBtn = document.querySelector(".app__card-button--foco");
+const shortPauseBtn = document.querySelector(".app__card-button--curto");
+const longPauseBtn = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+
+focusBtn.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "foco");
+});
+
+shortPauseBtn.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "descanso-curto");
+});
+
+longPauseBtn.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "descanso-longo");
+});

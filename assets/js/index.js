239182f72d20272eac730a13deaa1272e2587025
Hector Leubeck <hector.leubeck.lux.renatus@gmail.com>
2025-08-20 20:38:27 -0300

    perf: remove useless functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,15 +6,12 @@ const longPauseBtn = document.querySelector(".app__card-button--longo");
 
 focusBtn.addEventListener("click", () => {
   changeContext("focus");
-  setActiveButton(focusBtn);
 });
 
 shortPauseBtn.addEventListener("click", () => {
   changeContext("short-pause");
-  setActiveButton("short-pause");
 });
 
 longPauseBtn.addEventListener("click", () => {
   changeContext("long-pause");
-  setActiveButton("long-pause");
 });
